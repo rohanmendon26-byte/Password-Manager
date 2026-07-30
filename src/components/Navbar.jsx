1c215fc388a6f2a601a8350a11e443596df231df
Rohan Mendon <rohanmendon26@gmail.com>
2026-07-30 21:54:20 +0530

    completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-purple-200'>
+    <nav className='bg-slate-800 text-white'>
       <div className="mycontainer flex justify-between items-center px-4 py-5 h-14 ">
-      <div className="logo font-bold text-xl">Passop</div>
+      <div className="logo font-bold text-white text-2xl ">
+        <span className='text-green-700'>/ &lt;</span>
+        <span>Pass</span>
+        <span className='text-green-500'>OP/&gt;</span>
+         </div>
       <ul className='flex gap-4'>
         <li><a className='hover:font-bold transition' href="#">Home</a></li>
         <li><a className='hover:font-bold transition' href="#">About</a></li>
