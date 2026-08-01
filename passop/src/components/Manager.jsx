b54f48d7a3c3d6ed0ade25d0b42bcedb7e4b8f25
Rohan Mendon <rohanmendon26@gmail.com>
2026-08-01 11:00:07 +0530

    completed

--- passop/src/components/Manager.jsx
+++ passop/src/components/Manager.jsx
@@ -9,7 +9,7 @@ const Manager = () => {
     // =========================
     // API CONFIGURATION
     // =========================
-   const API_URL = `${import.meta.env.VITE_API_URL || 'http://localhost:3000/api'}/passwords`;
+    const API_URL = `${import.meta.env.VITE_API_URL || 'https://password-manager-3-zm4q.onrender.com'}/api/passwords`;
 
     // References
     const ref = useRef();
